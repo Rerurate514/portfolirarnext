cc5d3f08807c354949b47cc1c33c14a8ea5da2eb
Rerurate514 <rerurateyuto@gmail.com>
2025-07-23 15:49:48 +0900

    リンクを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div>
-          <p>ここがんばってつくる:現行のサイト → </p>
-          <a href="https://rerurate514.github.io/portfolirate"></a>
+          <p>ここがんばってつくる</p>
+          <a href="https://rerurate514.github.io/portfolirate">現行のサイト</a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
